92242cd62ac3c6bfb858ed7329317fab9894a614
Ting Li <kirk_lee789@hotmail.com>
2021-07-08 11:47:46 +0800

    singleton pattern

---
+++ SingletonPattern/Models/LoadBalancer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern.Models
+{
+    using System.Collections;
+    class LoadBalancer
+    {
+        private static readonly LoadBalancer instance = new LoadBalancer();
+
+        private ArrayList serverList = null;
+
+        private LoadBalancer()
+        {
+            serverList = new ArrayList();
+        }
+
+        public static LoadBalancer GetInstance()
+        {
+            return instance;
+        }
+
+        public void AddServer(string server)
+        {
+            serverList.Add(server);
+        }
+
+        public void Remove(string server)
+        {
+            serverList.Remove(server);
+        }
+
+        public string GetServer()
+        {
+            Random random = new Random();
+            int i = random.Next(serverList.Count);
+            return serverList[i].ToString();
+        }
+
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SingletonPattern
+{
+    using System;
+    using Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadBalancer lb1, lb2, lb3, lb4;
+
+            lb1 = LoadBalancer.GetInstance();
+            lb2 = LoadBalancer.GetInstance();
+            lb3 = LoadBalancer.GetInstance();
+            lb4 = LoadBalancer.GetInstance();
+
+            lb1.AddServer("Server1");
+            lb1.AddServer("Server2");
+            lb1.AddServer("Server3");
+            lb1.AddServer("Server4");
+
+            if (lb1 == lb2 && lb2 == lb3&& lb3 == lb4)
+            {
+                Console.WriteLine("Pass");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
