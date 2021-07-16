0563f2d8300117b6db69cc5e64d9ab139a037b45
Ting Li <kirk_lee789@hotmail.com>
2021-07-16 17:08:05 +0800

    Visitor Pattern

---
+++ VisitorPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisitorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// Not implement yet!
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
